d987ca2fff9b424f7117f3473871630595b10f04
JuzioMieio520 <mikolaj.olszowy@gmail.com>
2022-12-06 17:54:37 +0100

    refactor: format all files

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,55 @@
-import express, { Request, Response } from "express";
-import { Server } from "http";
-import checkEnv from "./utils/env";
-import logger from "./utils/logger";
-import prisma from "./utils/prisma";
+import express, { Request, Response } from 'express';
+import { Server } from 'http';
+import checkEnv from './utils/env';
+import logger from './utils/logger';
+import prisma from './utils/prisma';
 checkEnv();
 
 const app = express();
 app.use(express.json());
-app.get("/", (req: Request, res: Response) => {
-  res.json({
-    status: 200,
-    body: {
-      error: null,
-      message: "oh hi there",
-    },
-    meta: {
-      timestamp: new Date().toISOString(),
-      version: "a1.0.0",
-      server: "localhost",
-    },
-  });
+app.get('/', (req: Request, res: Response) => {
+    res.json({
+        status: 200,
+        body: {
+            error: null,
+            message: 'oh hi there',
+        },
+        meta: {
+            timestamp: new Date().toISOString(),
+            version: 'a1.0.0',
+            server: 'localhost',
+        },
+    });
 });
 
 prisma
-  .$connect()
-  .then(() => {
-    logger.info(`connected to database`);
-    const server = app.listen(process.env.PORT, () => {
-      logger.info(`server started on port ${process.env.PORT}`);
+    .$connect()
+    .then(() => {
+        logger.info(`connected to database`);
+        const server = app.listen(process.env.PORT, () => {
+            logger.info(`server started on port ${process.env.PORT}`);
+        });
+        process.once('SIGTERM', () => shutdown(server));
+        process.once('SIGINT', () => shutdown(server));
+    })
+    .catch((err) => {
+        logger.error(`failed to connect to database: ${err}`);
+        process.exit(0x000a);
     });
-    process.once("SIGTERM", () => shutdown(server));
-    process.once("SIGINT", () => shutdown(server));
-  })
-  .catch((err) => {
-    logger.error(`failed to connect to database: ${err}`);
-    process.exit(0x000a);
-  });
 
 function shutdown(server: Server) {
-  logger.info(`closing all open socket connections...`);
-  server.close(() => {
-    logger.info(`server closed, disconnecting from database...`);
-    prisma
-      .$disconnect()
-      .then(() => {
-        logger.info(`disconnected from database`);
-        process.exit(0x0003);
-      })
-      .catch((err) => {
-        logger.error(`failed to gracefully disconnect from database: ${err}`);
-        process.exit(0x000a);
-      });
-  });
+    logger.info(`closing all open socket connections...`);
+    server.close(() => {
+        logger.info(`server closed, disconnecting from database...`);
+        prisma
+            .$disconnect()
+            .then(() => {
+                logger.info(`disconnected from database`);
+                process.exit(0x0003);
+            })
+            .catch((err) => {
+                logger.error(`failed to gracefully disconnect from database: ${err}`);
+                process.exit(0x000a);
+            });
+    });
 }
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,40 +1,32 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
-require("dotenv").config();
+require('dotenv').config();
 
 export default function checkEnv() {
-  const schema = yup.object({
-    PORT: yup.number().required().min(0).max(65535),
-    ENV: yup
-      .string()
-      .required()
-      .matches(/(development|production)/),
-      LOG_FILES_DIR: yup
-        .string()
-        .default('logs'),
-        ENABLE_LOG_FILES: yup
-        .boolean()
-        .default(true),
+    const schema = yup.object({
+        PORT: yup.number().required().min(0).max(65535),
+        ENV: yup
+            .string()
+            .required()
+            .matches(/(development|production)/),
+        LOG_FILES_DIR: yup.string().default('logs'),
+        ENABLE_LOG_FILES: yup.boolean().default(true),
         FILE_LOG_LEVEL: yup
-        .string()
-        .matches(/(silly|debug|info|warn|error|critical)/)
-        .default('warn'),
+            .string()
+            .matches(/(silly|debug|info|warn|error|critical)/)
+            .default('warn'),
         CONSOLE_LOG_LEVEL: yup
-        .string()
-        .matches(/(silly|debug|info|warn|error|critical)/)
-        .default('info'),
-        EXPLICIT_DISABLE_CONSOLE_LOG: yup.boolean().default(false)
-  });
-try {
-    
-    schema.validateSync(process.env);
-    process.env = schema.cast(process.env) as any;
-    } catch(err:any) {
-    console.error(
-        "[CRTITICAL] invalid .env file structure, check README.md for more info"
-        );
+            .string()
+            .matches(/(silly|debug|info|warn|error|critical)/)
+            .default('info'),
+        EXPLICIT_DISABLE_CONSOLE_LOG: yup.boolean().default(false),
+    });
+    try {
+        schema.validateSync(process.env);
+        process.env = schema.cast(process.env) as any;
+    } catch (err: any) {
+        console.error('[CRTITICAL] invalid .env file structure, check README.md for more info');
         console.error(err.errors);
         process.exit(0x0004);
-
     }
 }
