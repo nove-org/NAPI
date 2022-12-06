10fc081a2bb59c0d8ed5dc26c758d75a0e12c93b
JuzioMieio520 <mikolaj.olszowy@gmail.com>
2022-12-06 17:20:17 +0100

    fix: graceful shutdown working

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-import checkEnv from "./utils/env";
-checkEnv();
 import express from "express";
+import checkEnv from "./utils/env";
 import logger from "./utils/logger";
+checkEnv();
 
 const app = express();
 app.use(express.json());
 
 const server = app.listen(process.env.PORT, () => {
-  logger.info("hi");
+  logger.info(`server started on port ${process.env.PORT}`);
 });
-// TODO: add some nice logging (winston or maybe custom?)
-process.on("SIGTERM", () => {
-  console.log("closing all open socket connections...");
+
+function shutdown() {
+  logger.info(`closing all open socket connections...`);
   server.close(() => {
-    console.log("server closed");
+    logger.info(`server closed`);
     process.exit(0x0003);
   });
-});
+}
+process.once("SIGTERM", () => shutdown());
+process.once("SIGINT", () => shutdown());
