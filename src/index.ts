72bc96731b6dc36810746e4cf52596d888e46428
JuzioMieio520 <mikolaj.olszowy@gmail.com>
2022-12-06 17:29:49 +0100

    feat: finish prisma setup and connection

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,41 @@
 import express from "express";
+import { Server } from "http";
 import checkEnv from "./utils/env";
 import logger from "./utils/logger";
+import prisma from "./utils/prisma";
 checkEnv();
 
 const app = express();
 app.use(express.json());
 
-const server = app.listen(process.env.PORT, () => {
-  logger.info(`server started on port ${process.env.PORT}`);
-});
+prisma
+  .$connect()
+  .then(() => {
+    logger.info(`connected to database`);
+    const server = app.listen(process.env.PORT, () => {
+      logger.info(`server started on port ${process.env.PORT}`);
+    });
+    process.once("SIGTERM", () => shutdown(server));
+    process.once("SIGINT", () => shutdown(server));
+  })
+  .catch((err) => {
+    logger.error(`failed to connect to database: ${err}`);
+    process.exit(0x000a);
+  });
 
-function shutdown() {
+function shutdown(server: Server) {
   logger.info(`closing all open socket connections...`);
   server.close(() => {
-    logger.info(`server closed`);
-    process.exit(0x0003);
+    logger.info(`server closed, disconnecting from database...`);
+    prisma
+      .$disconnect()
+      .then(() => {
+        logger.info(`disconnected from database`);
+        process.exit(0x0003);
+      })
+      .catch((err) => {
+        logger.error(`failed to gracefully disconnect from database: ${err}`);
+        process.exit(0x000a);
+      });
   });
 }
-process.once("SIGTERM", () => shutdown());
-process.once("SIGINT", () => shutdown());
