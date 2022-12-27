de808baee622f6f284f99b4689d8d8236a93e613
JuzioMieio520 <mikolaj.olszowy@gmail.com>
2022-12-27 18:13:15 +0100

    feat: oauth2 finish (add /token?refresh_token)

--- src/routes/v1/oauth2.ts
+++ src/routes/v1/oauth2.ts
@@ -29,6 +29,8 @@ router.get(
         });
         // TODO: visually pleasing error page
         if (!client) return res.status(400).send('invalid client_id');
+        if (!client.redirect_uris.includes(req.query.redirect_uri as string)) return res.status(400).send('invalid redirect_uri');
+        if (req.query.response_type !== 'code') return res.status(400).send('invalid response_type');
 
         res.render('v1/oauth2/authorize', {
             client,
@@ -82,17 +84,20 @@ router.post(
         z.object({
             client_id: z.string().min(1).max(64),
             scope: z.string().min(1).max(1024),
-            code: z.string().min(1).max(1024),
-            redirect_uri: z.string().min(1).max(1024),
+            code: z.string().min(1).max(1024).optional(),
+            refresh_token: z.string().min(1).max(1024).optional(),
+            redirect_uri: z.string().min(1).max(1024).optional(),
             grant_type: z.string().min(1).max(1024),
             client_secret: z.string().min(1).max(1024),
         }),
         'query'
     ),
     async (req: Request, res: Response) => {
+        const TOKEN_LIFETIME = 3600 * 1000;
+
         const client = await prisma.oAuth_App.findFirst({
             where: {
-                client_id: req.body.client_id?.toString() || '',
+                client_id: req.query.client_id?.toString() || '',
             },
         });
         if (!client) return res.status(400).send('invalid client_id');
@@ -100,34 +105,75 @@ router.post(
         if (!client.redirect_uris.includes(req.query.redirect_uri?.toString() as string)) return res.status(400).send('invalid redirect_uri');
         if (req.query.grant_type?.toString() !== 'authorization_code') return res.status(400).send('invalid grant_type');
 
-        const code = await prisma.oAuth_Code.findFirst({
-            where: {
-                code: req.query.code?.toString() || '',
-            },
-        });
-        if (!code) return res.status(400).send('invalid code');
-        if (code.app_id !== client.client_id) return res.status(400).send('invalid code');
-        if (code.scopes.join(' ') !== req.query.scope?.toString()) return res.status(400).send('invalid scope');
+        if (req.query.code?.toString()) {
+            const code = await prisma.oAuth_Code.findFirst({
+                where: {
+                    code: req.query.code?.toString() || '',
+                },
+            });
+            if (!code) return res.status(400).send('invalid code');
+            if (code.app_id !== client.client_id) return res.status(400).send('invalid code');
+            if (code.scopes.join(' ') !== req.query.scope?.toString()) return res.status(400).send('invalid scope');
 
-        const authorization = await prisma.oAuth_Authorization.create({
-            data: {
-                user_id: code.user_id,
-                app_id: client.client_id,
-                scopes: code.scopes,
-                redirect_uri: req.query.redirect_uri?.toString() || '',
-                token: randomString(64),
-                token_expires: new Date(Date.now() + 3600 * 1000),
-                refresh_token: randomString(64),
-            },
-        });
+            const authorization = await prisma.oAuth_Authorization.create({
+                data: {
+                    user_id: code.user_id,
+                    app_id: client.client_id,
+                    scopes: code.scopes,
+                    redirect_uri: req.query.redirect_uri?.toString() || '',
+                    token: randomString(64),
+                    token_expires: new Date(Date.now() + TOKEN_LIFETIME),
+                    refresh_token: randomString(64),
+                },
+            });
+            await prisma.oAuth_Code.delete({
+                where: {
+                    code: req.query.code?.toString() || '',
+                },
+            });
 
-        createResponse(res, 200, {
-            access_token: authorization.token,
-            token_type: 'Bearer',
-            expires_in: authorization.token_expires.getTime() - Date.now(),
-            scope: encodeURIComponent(authorization.scopes.join(' ')),
-            refresh_token: authorization.refresh_token,
-        });
+            createResponse(res, 200, {
+                access_token: authorization.token,
+                token_type: 'Bearer',
+                expires_in: authorization.token_expires.getTime() - Date.now(),
+                scope: encodeURIComponent(authorization.scopes.join(' ')),
+                refresh_token: authorization.refresh_token,
+            });
+        } else if (req.query.refresh_token?.toString()) {
+            let authorization = await prisma.oAuth_Authorization.findFirst({
+                where: {
+                    refresh_token: req.query.refresh_token?.toString() || '',
+                },
+            });
+            if (!authorization) return res.status(400).send('invalid refresh_token');
+            if (authorization.app_id !== client.client_id) return res.status(400).send('invalid refresh_token');
+            if (authorization.scopes.join(' ') !== req.query.scope?.toString()) return res.status(400).send('invalid scope');
+
+            authorization.token = randomString(64);
+            authorization.token_expires = new Date(Date.now() + TOKEN_LIFETIME);
+            authorization.refresh_token = randomString(64);
+
+            authorization = await prisma.oAuth_Authorization.update({
+                where: {
+                    id: authorization.id,
+                },
+                data: {
+                    token: randomString(64),
+                    token_expires: new Date(Date.now() + TOKEN_LIFETIME),
+                    refresh_token: randomString(64),
+                },
+            });
+
+            createResponse(res, 200, {
+                access_token: authorization.token,
+                token_type: 'Bearer',
+                expires_in: authorization.token_expires.getTime() - Date.now(),
+                scope: encodeURIComponent(authorization.scopes.join(' ')),
+                refresh_token: authorization.refresh_token,
+            });
+        } else {
+            return res.status(400).send('code or refresh_token required');
+        }
     }
 );
 
