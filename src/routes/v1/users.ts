e05c6d81666a065312df8d3011585531ec1bec33
dejwidson2137 <dawid.szewczyk678@gmail.com>
2023-02-26 21:24:52 +0100

    fix: user account edit

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -87,22 +87,13 @@ router.patch(
     validate(z.object({ username: z.string().min(1).max(24).optional(), bio: z.string().min(1).max(256).optional() })),
     authorizeOwner,
     async (req: Request, res: Response) => {
-        if (req.body?.bio) {
-            await prisma.user.update({
-                where: { id: req.user.id },
-                data: {
-                    bio: req.body?.bio,
-                },
-            });
-        }
-        if (req.body?.username) {
-            await prisma.user.update({
-                where: { id: req.user.id },
-                data: {
-                    username: req.body?.username,
-                },
-            });
-        }
+        await prisma.user.update({
+            where: { id: req.user.id },
+            data: {
+                bio: req.body?.bio,
+                username: req.body?.username,
+            },
+        });
 
         return createResponse(res, 200, removeProps(req.user, ['password', 'token']));
     }
