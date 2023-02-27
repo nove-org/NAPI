a4798cd0732cf23cd9507617f580c983fade64b6
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-02-27 21:40:14 +0000

    feat: error handling page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import v1 from './v1';
+import error from './error';
 
 const router = Router();
 
+router.use('/error', error);
 router.use('/v1', v1);
 
 export default router;
