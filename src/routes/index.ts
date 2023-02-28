09f57194561140e55a821ba55a7179c96ce2b185
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-02-28 15:29:58 +0000

    feat: confirm email page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import v1 from './v1';
-import error from './error';
+import tests from './tests';
 
 const router = Router();
 
 router.use('/v1', v1);
 
 //! This is just for testing purposes, please change it ASAP to proper error handler
-router.use('/error', error);
+router.use('/tests', tests);
 
 export default router;
