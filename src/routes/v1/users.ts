9bbee49e530c8e9c09b8cfb112791c3b0e5074ff
dejwidson2137 <dawid.szewczyk678@gmail.com>
2023-03-09 18:58:47 +0100

    fix: the password reset code expires after 3 days

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -82,6 +82,11 @@ router.get('/passwordKey', async (req: Request, res: Response) => {
 
     if (!recovery) return createError(res, 404, { code: 'invalid_code', message: 'invalid password recovery code', param: 'query:code', type: 'authorization' });
 
+    if (recovery.expiresAt.getTime() < Date.now()) {
+        await prisma.recovery.delete({ where: { code: recovery.code } });
+        return createError(res, 404, { code: 'invalid_code', message: 'invalid password recovery code', param: 'query:code', type: 'authorization' });
+    }
+
     return createResponse(res, 200, { recoveryKey: code });
 });
 
