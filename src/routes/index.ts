c43b0d641a6a308df9182e7ee5a0093ee4a055e9
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-04-30 08:22:25 +0000

    chore:  code optimization

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import v1 from './v1';
-import localStorage from './localStorage';
 
 const router = Router();
 
 router.use('/v1', v1);
-router.use('/ls', localStorage); //! hotfix for not accessible localStorage
 
 export default router;
