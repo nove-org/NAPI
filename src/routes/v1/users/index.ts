855bf0978d017442be5062676f6e433beb64bb96
wnm210 <matt@nove.team>
2023-09-04 10:40:55 +0000

    fix: include emailReset router in /users index

--- src/routes/v1/users/index.ts
+++ src/routes/v1/users/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import auth from './auth';
 import me from './me';
 import passwordReset from './passwordReset';
+import emailReset from './emailReset';
 import query from './query';
 
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 router.use('/', auth);
 router.use('/', me);
 router.use('/', passwordReset);
+router.use('/', emailReset);
 router.use('/', query);
 
 export default router;
