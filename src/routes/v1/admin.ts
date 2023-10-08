14748d8aaa696ec80b8003816bfa509418b86413
dawid <dawid@nove.team>
2023-10-08 21:46:05 +0200

    chore: password authorization for methods from the admin router

--- src/routes/v1/admin.ts
+++ src/routes/v1/admin.ts
@@ -9,13 +9,14 @@ import createResponse from '@util/createResponse';
 import { validate } from '@util/schema';
 import { z } from 'zod';
 import nodemailer from 'nodemailer';
+import { checkAdminPassword } from '@middleware/checkAdminPassword';
 interface UserAvatar extends User {
     avatar: string;
 }
 
 const router = Router();
 
-router.get('/users', authorize({ disableBearer: true }), authorizeAdmin, async (req: Request, res: Response) => {
+router.post('/users', authorize({ disableBearer: true }), authorizeAdmin, checkAdminPassword, async (req: Request, res: Response) => {
     const usersDB = await prisma.user.findMany({ orderBy: { createdAt: 'desc' } });
 
     let users: Partial<UserAvatar>[] = [];
@@ -35,75 +36,89 @@ router.get('/users', authorize({ disableBearer: true }), authorizeAdmin, async (
     return createResponse(res, 200, users);
 });
 
-router.patch('/users/:id/delete', authorize({ disableBearer: true }), authorizeAdmin, validate(z.object({ reason: z.string() })), async (req: Request, res: Response) => {
-    const { id } = req.params;
+router.patch(
+    '/users/:id/delete',
+    authorize({ disableBearer: true }),
+    authorizeAdmin,
+    checkAdminPassword,
+    validate(z.object({ reason: z.string() })),
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
 
-    const user = await prisma.user.findFirst({ where: { id } });
+        const user = await prisma.user.findFirst({ where: { id } });
 
-    if (!user) return createError(res, 400, { code: 'invalid_id', message: 'This user does not exist!', type: 'validation', param: 'param:id' });
+        if (!user) return createError(res, 400, { code: 'invalid_id', message: 'This user does not exist!', type: 'validation', param: 'param:id' });
 
-    if (!req.body.reason?.length) return createError(res, 400, { code: 'invalid_reason', message: 'You have to provide a reason', type: 'validation', param: 'body:reason' });
+        if (!req.body.reason?.length) return createError(res, 400, { code: 'invalid_reason', message: 'You have to provide a reason', type: 'validation', param: 'body:reason' });
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: 465,
-        tls: {
-            rejectUnauthorized: false,
-        },
-        auth: {
-            user: process.env.MAIL_USERNAME,
-            pass: process.env.MAIL_PASSWORD,
-        },
-    });
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: 465,
+            tls: {
+                rejectUnauthorized: false,
+            },
+            auth: {
+                user: process.env.MAIL_USERNAME,
+                pass: process.env.MAIL_PASSWORD,
+            },
+        });
 
-    //TODO: HTML to email
-    await transporter.sendMail({
-        from: process.env.MAIL_USERNAME,
-        to: user.email,
-        subject: 'account deleted',
-        html: `your account has been deleted because of ${req.body.reason}, this action was triggered by: ${req.user.username}`,
-    });
+        //TODO: HTML to email
+        await transporter.sendMail({
+            from: process.env.MAIL_USERNAME,
+            to: user.email,
+            subject: 'account deleted',
+            html: `your account has been deleted because of ${req.body.reason}, this action was triggered by: ${req.user.username}`,
+        });
 
-    await prisma.user.delete({ where: { id } });
+        await prisma.user.delete({ where: { id } });
 
-    return createResponse(res, 200, { success: true });
-});
+        return createResponse(res, 200, { success: true });
+    }
+);
 
-router.post('/users/:id/disable', authorize({ disableBearer: true }), authorizeAdmin, validate(z.object({ reason: z.string() })), async (req: Request, res: Response) => {
-    const { id } = req.params;
+router.post(
+    '/users/:id/disable',
+    authorize({ disableBearer: true }),
+    authorizeAdmin,
+    checkAdminPassword,
+    validate(z.object({ reason: z.string() })),
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
 
-    const user = await prisma.user.findFirst({ where: { id } });
+        const user = await prisma.user.findFirst({ where: { id } });
 
-    if (!user) return createError(res, 400, { code: 'invalid_id', message: 'This user does not exist!', type: 'validation', param: 'param:id' });
+        if (!user) return createError(res, 400, { code: 'invalid_id', message: 'This user does not exist!', type: 'validation', param: 'param:id' });
 
-    if (!req.body.reason?.length) return createError(res, 400, { code: 'invalid_reason', message: 'You have to provide a reason', type: 'validation', param: 'body:reason' });
+        if (!req.body.reason?.length) return createError(res, 400, { code: 'invalid_reason', message: 'You have to provide a reason', type: 'validation', param: 'body:reason' });
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: 465,
-        tls: {
-            rejectUnauthorized: false,
-        },
-        auth: {
-            user: process.env.MAIL_USERNAME,
-            pass: process.env.MAIL_PASSWORD,
-        },
-    });
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: 465,
+            tls: {
+                rejectUnauthorized: false,
+            },
+            auth: {
+                user: process.env.MAIL_USERNAME,
+                pass: process.env.MAIL_PASSWORD,
+            },
+        });
 
-    //TODO: HTML to email
-    await transporter.sendMail({
-        from: process.env.MAIL_USERNAME,
-        to: user.email,
-        subject: 'account disabled',
-        html: `your account has been disabled because of ${req.body.reason}, this action was triggered by: ${req.user.username}`,
-    });
+        //TODO: HTML to email
+        await transporter.sendMail({
+            from: process.env.MAIL_USERNAME,
+            to: user.email,
+            subject: 'account disabled',
+            html: `your account has been disabled because of ${req.body.reason}, this action was triggered by: ${req.user.username}`,
+        });
 
-    await prisma.user.update({ where: { id }, data: { disabled: true } });
+        await prisma.user.update({ where: { id }, data: { disabled: true } });
 
-    return createResponse(res, 200, { success: true });
-});
+        return createResponse(res, 200, { success: true });
+    }
+);
 
-router.delete('/users/:id/disable', authorize({ disableBearer: true }), authorizeAdmin, async (req: Request, res: Response) => {
+router.delete('/users/:id/disable', authorize({ disableBearer: true }), authorizeAdmin, checkAdminPassword, async (req: Request, res: Response) => {
     const { id } = req.params;
 
     const user = await prisma.user.findFirst({ where: { id } });
