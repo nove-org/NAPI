f4e4b3cf2008806d1bf3a1583630f8dbf085c1fc
wnm210 <matt@nove.team>
2024-07-12 12:27:21 +0000

    fix: add missing lang support

--- src/utils/CONSTS.ts
+++ src/utils/CONSTS.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 
 export const STORAGE_PATH = join(__dirname, '../../storage/users/avatars/');
-export const AVAILABLE_LANGUAGES = ['pl-PL', 'sw-PL', 'en-US', 'en-GB', 'nl-NL', 'fr-FR', 'uk-UA', 'hu-HU', 'ja-JP'];
+export const AVAILABLE_LANGUAGES = ['pl-PL', 'en-US', 'en-GB', 'nl-NL', 'fr-FR', 'uk-UA', 'hu-HU', 'ja-JP', 'ru-RU', 'bg-BG'];
 export const AVAILABLE_LANGUAGES_REGEX = new RegExp(`^(${AVAILABLE_LANGUAGES.join('|')})$`);
 
 export const RATELIMIT_IP_WHITELIST: string[] = [];
